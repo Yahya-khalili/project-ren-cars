2fe00fefe54867a7452af41584479976497639ab
yahya khalili <khalilioyoyooy@gmail.com>
2024-03-28 00:57:57 +0000

    MESSAGE

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,6 @@ class User extends Authenticatable
         'fullName',
         'cin',
         'email',
-        'password',
         'phone',
     ];
 
--- resources/views/pages/users/user.blade.php
+++ resources/views/pages/users/user.blade.php
@@ -50,10 +50,7 @@
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                             <input type="email" name="email" id="email"   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com"  />
                         </div>
-                        <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password"   id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  />
-                        </div>
+                       
                         
                         <div>
                             <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
